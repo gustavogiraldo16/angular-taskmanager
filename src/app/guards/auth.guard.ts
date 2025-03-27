3f612b68f2073073c8167bdf4451f4e29ba3fc40
Gustavo Giraldo <gustavoadolfogiraldorendon@gmail.com>
2025-03-27 07:56:51 -0500

    Add AuthGuard to protect routes and implement token validation

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      this.router.navigate(['/login']); // Si no hay token, redirigir al login
+      return false;
+    }
+    console.log('Token:', token);
+    return true; // Si hay token, permite el acceso
+  }
+}
