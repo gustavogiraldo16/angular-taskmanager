c24ea624ba57e11238ac37cb58cbe6fee423488e
Gustavo Giraldo <gustavoadolfogiraldorendon@gmail.com>
2025-03-27 07:56:30 -0500

    Add HTTP client and configure routes for login and tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'tasks', component: TasksComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'login' }, // Redirecciona cualquier ruta no encontrada al login por ahora
+];
